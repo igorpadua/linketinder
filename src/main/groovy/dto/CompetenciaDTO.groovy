a7556d17bcaabefc033e6c2e91638217fb76a717
igorpadua <igorpadua@tutanota.com>
2023-02-27 15:34:17 -0300

    fix: Variavel escrito errado

--- src/main/groovy/dto/CompetenciaDTO.groovy
+++ src/main/groovy/dto/CompetenciaDTO.groovy
@@ -19,7 +19,7 @@ class CompetenciaDTO {
 
     static int getIdCompetencia(String competencia) {
         Sql sql = Sql.newInstance(url, user, password, drive)
-        List result = sql.rows("SELECT id FROM competencias WHERE competencia = ${competencias}")
+        List result = sql.rows("SELECT id FROM competencias WHERE competencia = ${competencia}")
         sql.close()
         return result[0].id as int
     }
