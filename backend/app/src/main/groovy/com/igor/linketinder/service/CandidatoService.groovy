baaea00b4b0362f6784169d43bb2f1e76b351c95
igorpadua <igorpadua@tutanota.com>
2023-03-08 11:29:07 -0300

    refactor: Trata a data invalida no nascimento

--- backend/app/src/main/groovy/com/igor/linketinder/service/CandidatoService.groovy
+++ backend/app/src/main/groovy/com/igor/linketinder/service/CandidatoService.groovy
@@ -8,24 +8,34 @@ import java.text.SimpleDateFormat
 
 @TypeChecked
 class CandidatoService {
+
+    static Date pegaNascimento() {
+        Scanner scanner = new Scanner(System.in)
+        print("Digite a data de nascimento do candidato: ")
+        String nascimento = scanner.nextLine()
+        try {
+            Date data = new SimpleDateFormat("dd/MM/yyyy").parse(nascimento)
+            return data
+        } catch (Exception ignored) {
+            println("Data inválida")
+            return pegaNascimento()
+        }
+    }
+
     static Candidato newCandidato() {
         Scanner scanner = new Scanner(System.in)
         print("Digite o nome do novo candidato: ")
         String nome = scanner.nextLine()
         print("Digite o sobrenome do novo candidato: ")
         String sobrenome = scanner.nextLine()
-        print("Digite a data de nascimento do candidato: ")
-        String nascimento = scanner.nextLine()
-        // Transforma a data de String para Date
-        Date data = new SimpleDateFormat("dd/MM/yyyy").parse(nascimento)
+        Date nascimento = pegaNascimento()
         print("Digite o email do candidato: ")
         String email = scanner.nextLine()
         print("Digite o CPF do candidato: ")
         String cpf = scanner.nextLine()
         print("Digite o país do candidato: ")
         String pais = scanner.nextLine()
-        print("Digite o CEP  do candidato: ")
-        String cep = scanner.nextLine()
+        String cep = PessoaService.pegaCep()
         print("Digite uma descrição do candidato: ")
         String desc = scanner.nextLine()
         print("Digite a senha do candidato: ")
@@ -33,7 +43,7 @@ class CandidatoService {
 
         List<Competencia> competencias = CompetenciaService.choiseCompetencia()
 
-        return new Candidato(nome, sobrenome, data, email, cpf, pais, cep, desc, senha,competencias)
+        return new Candidato(nome, sobrenome, nascimento, email, cpf, pais, cep, desc, senha,competencias)
     }
 
     static void printCandidatos(List<Candidato> pessoas) {
--- backend/app/src/test/groovy/com/igor/linketinder/CandidatoServiceTest.groovy
+++ backend/app/src/test/groovy/com/igor/linketinder/CandidatoServiceTest.groovy
@@ -4,7 +4,8 @@ import com.igor.linketinder.entity.Candidato
 import com.igor.linketinder.entity.Competencia
 import groovy.transform.TypeChecked
 import org.junit.jupiter.api.Test
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.assertEquals
 
 @TypeChecked
 class CandidatoServiceTest {
@@ -13,6 +14,6 @@ class CandidatoServiceTest {
         List<Candidato> pessoas = new ArrayList<>()
         pessoas.add(new Candidato('Joao', 'Silva', new Date(2000-04-23), 'joao@gmail.com', '000.000.000-00', 'Brasil', '00000-00', 'Sou um candidato', '123456', [Competencia.cplusplus, Competencia.Java]))
         Candidato resul = new Candidato('Joao', 'Silva', new Date(2000-04-23), 'joao@gmail.com', '000.000.000-00', 'Brasil', '00000-00', 'Sou um candidato', '123456', [Competencia.cplusplus, Competencia.Java])
-        assertEquals(pessoas.get(0), resul);
+        assertEquals(pessoas.get(0), resul)
     }
 }
