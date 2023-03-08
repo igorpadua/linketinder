dfad5e803e57ffba971d968831cb961a50224ce1
igorpadua <igorpadua@tutanota.com>
2023-03-08 10:48:50 -0300

    Test: Valida cep

---
+++ backend/app/src/test/groovy/com/igor/linketinder/service/PessoaServiceTest.groovy
@@ -0,0 +1,36 @@
+package com.igor.linketinder.service
+
+import org.junit.jupiter.api.Test
+
+import static org.junit.jupiter.api.Assertions.assertEquals
+
+class PessoaServiceTest {
+
+    @Test
+    void validacepTest() {
+        String cep = "00000-000"
+        Boolean resul = PessoaService.validaCEP(cep)
+        assertEquals(true, resul)
+    }
+
+    @Test
+    void cepInvalidoLetraTest() {
+        String cep = "00000-0A0"
+        Boolean resul = PessoaService.validaCEP(cep)
+        assertEquals(false, resul)
+    }
+
+    @Test
+    void cepInvalidoPequenoTest() {
+        String cep = "0000-00"
+        Boolean resul = PessoaService.validaCEP(cep)
+        assertEquals(false, resul)
+    }
+
+    @Test
+    void cepNulo() {
+        String cep = null
+        Boolean resul = PessoaService.validaCEP(cep)
+        assertEquals(false, resul)
+    }
+}
