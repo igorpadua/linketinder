d0c791c55a8db5502b64793b0b521c8b7f08f5b1
igorpadua <igorpadua@tutanota.com>
2023-03-08 12:14:14 -0300

    refactor: criado VagaController

--- backend/app/src/main/groovy/com/igor/linketinder/App.groovy
+++ backend/app/src/main/groovy/com/igor/linketinder/App.groovy
@@ -2,18 +2,7 @@ package com.igor.linketinder
 
 import com.igor.linketinder.controller.CandidatoController
 import com.igor.linketinder.controller.EmpresaController
-import com.igor.linketinder.dao.CandidatoDAO
-import com.igor.linketinder.dao.CompetenciaCandidatoDAO
-import com.igor.linketinder.dao.CompetenciaVagasDAO
-import com.igor.linketinder.dao.EmpresaDAO
-import com.igor.linketinder.dao.VagaDAO
-import com.igor.linketinder.entity.Candidato
-import com.igor.linketinder.entity.Empresa
-import com.igor.linketinder.entity.Vaga
-import com.igor.linketinder.service.CandidatoService
-import com.igor.linketinder.service.EmpresaService
-import com.igor.linketinder.service.PessoaService
-import com.igor.linketinder.service.VagaService
+import com.igor.linketinder.controller.VagaController
 
 class App {
     static private void menu() {
@@ -33,10 +22,10 @@ class App {
     }
 
     static void main(String[] args) {
-        Boolean end = true
+        Boolean finalizaMenuPrincipal = true
         Scanner scanner = new Scanner(System.in)
 
-        while (end) {
+        while (finalizaMenuPrincipal) {
             menu()
             int opc = scanner.nextInt()
             switch (opc) {
@@ -47,15 +36,7 @@ class App {
                     EmpresaController.adicionar()
                     break
                 case 3:
-                    // Adicionar uma vaga
-                    final String cnpj = EmpresaService.pegaCnpj()
-                    // Pega o ID
-                    final int id = EmpresaDAO.getIdEmpresa(cnpj)
-                    Vaga vaga = VagaService.adicionaVaga()
-                    VagaDAO.inserirVaga(vaga, id)
-                    // Adiciona as competencias da vaga
-                    CompetenciaVagasDAO.inserirCompetenciaVaga(vaga, id)
-                    println("\nAdicionado com sucesso\n")
+                    VagaController.adicionar()
                     break
                 case 4:
                     CandidatoController.atualizar()
@@ -64,19 +45,7 @@ class App {
                     EmpresaController.atualizar()
                     break
                 case 6:
-                    // Atualizar uma vaga
-                    final String nome = VagaService.pegaNomeVaga()
-                    final String cnpj = EmpresaService.pegaCnpj()
-                    // Pega o ID Empresa
-                    final int id = EmpresaDAO.getIdEmpresa(cnpj)
-                    // Pega a vaga do banco de dados
-                    Vaga vaga = VagaDAO.getVaga(nome, id)
-                    // Atualiza a vaga
-                    VagaService.atualizarVaga(vaga)
-                    // Atualiza a vaga no banco de dados
-                    VagaDAO.atualizarVaga(vaga, nome, id)
-                    // Atualiza as competencias da vaga
-                    CompetenciaVagasDAO.atualizarCompetenciaVaga(vaga, id)
+                    VagaController.atualizar()
                     break
                 case 7:
                     CandidatoController.remover()
@@ -85,16 +54,7 @@ class App {
                     EmpresaController.remover()
                     break
                 case 9:
-                    // Remover uma vaga
-                    final String nome = VagaService.pegaNomeVaga()
-                    final String cnpj = EmpresaService.pegaCnpj()
-                    // Pega o ID Empresas
-                    final int id = EmpresaDAO.getIdEmpresa(cnpj)
-                    // Pega o ID Vagas
-                    final int idVaga = VagaDAO.getIdVaga(nome, id)
-                    CompetenciaVagasDAO.removeCompetenciaVaga(idVaga)
-                    VagaDAO.removeVaga(nome, id)
-                    println("\nRemovido com sucesso\n")
+                    VagaController.remover()
                     break
                 case 10:
                     CandidatoController.listar()
@@ -103,11 +63,10 @@ class App {
                     EmpresaController.listar()
                     break
                 case 12:
-                    // Listar vagas
-                    VagaService.printVagas(VagaDAO.listarVagas())
+                    VagaController.listar()
                     break
                 case 13:
-                    end = false
+                    finalizaMenuPrincipal = false
                     println("Saiu com sucesso")
                     break
                 default:
---
+++ backend/app/src/main/groovy/com/igor/linketinder/controller/VagaController.groovy
@@ -0,0 +1,56 @@
+package com.igor.linketinder.controller
+
+import com.igor.linketinder.dao.CompetenciaVagasDAO
+import com.igor.linketinder.dao.EmpresaDAO
+import com.igor.linketinder.dao.VagaDAO
+import com.igor.linketinder.entity.Vaga
+import com.igor.linketinder.service.EmpresaService
+import com.igor.linketinder.service.VagaService
+
+class VagaController {
+    static void adicionar() {
+        // Adicionar uma vaga
+        final String cnpj = EmpresaService.pegaCnpj()
+        // Pega o ID
+        final int id = EmpresaDAO.getIdEmpresa(cnpj)
+        Vaga vaga = VagaService.adicionaVaga()
+        VagaDAO.inserirVaga(vaga, id)
+        // Adiciona as competencias da vaga
+        CompetenciaVagasDAO.inserirCompetenciaVaga(vaga, id)
+        println("\nAdicionado com sucesso\n")
+    }
+
+    static void atualizar() {
+        // Atualizar uma vaga
+        final String nome = VagaService.pegaNomeVaga()
+        final String cnpj = EmpresaService.pegaCnpj()
+        // Pega o ID Empresa
+        final int id = EmpresaDAO.getIdEmpresa(cnpj)
+        // Pega a vaga do banco de dados
+        Vaga vaga = VagaDAO.getVaga(nome, id)
+        // Atualiza a vaga
+        VagaService.atualizarVaga(vaga)
+        // Atualiza a vaga no banco de dados
+        VagaDAO.atualizarVaga(vaga, nome, id)
+        // Atualiza as competencias da vaga
+        CompetenciaVagasDAO.atualizarCompetenciaVaga(vaga, id)
+    }
+
+    static void remover() {
+        // Remover uma vaga
+        final String nome = VagaService.pegaNomeVaga()
+        final String cnpj = EmpresaService.pegaCnpj()
+        // Pega o ID Empresas
+        final int id = EmpresaDAO.getIdEmpresa(cnpj)
+        // Pega o ID Vagas
+        final int idVaga = VagaDAO.getIdVaga(nome, id)
+        CompetenciaVagasDAO.removeCompetenciaVaga(idVaga)
+        VagaDAO.removeVaga(nome, id)
+        println("\nRemovido com sucesso\n")
+    }
+
+    static void listar() {
+        // Listar todas as vagas
+        VagaDAO.listarVagas()
+    }
+}
