8f1db8c62086d1e98b4842e3e9d3a455865bc49f
igorpadua <igorpadua@tutanota.com>
2023-03-08 16:19:50 -0300

    refactor: Valida está mais simples

--- frondend/src/validacao/validarPessoa.ts
+++ frondend/src/validacao/validarPessoa.ts
@@ -2,36 +2,48 @@ import Pessoa from "../pessoa/entities/pessoa.entity";
 import Candidato from "../pessoa/entities/candidato.entity";
 import Empresa from "../pessoa/entities/empresa.entity";
 
+const nomeRegex: RegExp = /^[a-zA-Z ]{2,30}$/
+const emailRegex: RegExp = /^\S+@\w+\.\w{2,6}(\.\w{2})?$/
+const descRegex: RegExp = /^(?=.*[A-Za-z])([0-9])*.{3,100}$/
+const cepRegex: RegExp = /^\d{5}-\d{3}$/
+const cpfRegex: RegExp = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/
+const cnpjRegex: RegExp = /^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/
+const idadeRegex: RegExp = /^\d{2}$/
+
+function dadosPreenchidosCandidato(candidato: Candidato) : boolean {
+    return !(candidato.cpf && candidato.idade)
+}
+
+function dadosPreenchidosEmpresa(empresa: Empresa) : boolean {
+    return !(empresa.cnpj)
+}
+
+function dadosForamPreenchidos(pessoa: Pessoa) : boolean {
+    return !(pessoa.nome && pessoa.email && pessoa.desc && pessoa.pais && pessoa.estado && pessoa.cidade && pessoa.cep)
+        || (pessoa instanceof Candidato && dadosPreenchidosCandidato(pessoa))
+        || (pessoa instanceof Empresa && dadosPreenchidosEmpresa(pessoa))
+}
+
+function validarCandidato(candidato: Candidato) : boolean {
+    if (!cpfRegex.test(candidato.cpf)) {
+        alert("CPF inválido!")
+        return false
+    }
+    if (!idadeRegex.test(candidato.idade.toString())) {
+        alert("Idade inválida!")
+        return false
+    }
+    return true
+}
+
 export default function validaPessoa(pessoa: Pessoa) {
 
-    if (!(pessoa.nome && pessoa.email && pessoa.desc && pessoa.pais && pessoa.estado && pessoa.cidade && pessoa.cep && pessoa.competencias.toString())) {
+    if (dadosForamPreenchidos(pessoa)) {
         alert("Preencha todos os campos!")
         return false
     }
 
-    if (pessoa instanceof Candidato) {
-        if (!(pessoa as Candidato).cpf && !(pessoa as Candidato).idade) {
-            alert("Preencha todos os campos!")
-            return false
-        }
-    }
-
-    if (pessoa instanceof Empresa) {
-        if (!(pessoa as Empresa).cnpj) {
-            alert("Preencha todos os campos!")
-            return false
-        }
-    }
-
-    const genericRegex: RegExp = /^[a-zA-Z ]{2,30}$/
-    const emailRegex: RegExp = /^\S+@\w+\.\w{2,6}(\.\w{2})?$/
-    const descRegex: RegExp = /^(?=.*[A-Za-z])([0-9])*.{3,100}$/
-    const cepRegex: RegExp = /^\d{5}-\d{3}$/
-    const cpfRegex: RegExp = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/
-    const cnpjRegex: RegExp = /^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/
-    const idadeRegex: RegExp = /^\d{2}$/
-
-    if (!genericRegex.test(pessoa.nome)) {
+    if (!nomeRegex.test(pessoa.nome)) {
         alert("Nome inválido!")
         return false
     }
@@ -43,31 +55,29 @@ export default function validaPessoa(pessoa: Pessoa) {
         alert("Descrição inválida!")
         return false
     }
+
     if (pessoa instanceof Candidato) {
-        if (!cpfRegex.test((pessoa as Candidato).cpf)) {
-            alert("CPF inválido!")
-            return false
-        }
-        if (!idadeRegex.test((pessoa as Candidato).idade.toString())) {
-            alert("Idade inválida!")
+        if (!validarCandidato(pessoa)) {
             return false
         }
     }
+
     if (pessoa instanceof Empresa) {
-        if (!cnpjRegex.test((pessoa as Empresa).cnpj)) {
+        if (!cnpjRegex.test(pessoa.cnpj)) {
             alert("CNPJ inválido!")
             return false
         }
     }
-    if (!genericRegex.test(pessoa.pais)) {
+
+    if (!nomeRegex.test(pessoa.pais)) {
         alert("País inválido!")
         return false
     }
-    if (!genericRegex.test(pessoa.estado)) {
+    if (!nomeRegex.test(pessoa.estado)) {
         alert("Estado inválido!")
         return false
     }
-    if (!genericRegex.test(pessoa.cidade)) {
+    if (!nomeRegex.test(pessoa.cidade)) {
         alert("Cidade inválida!")
         return false
     }
