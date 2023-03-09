20af1aaf7773a634fb29099bf00b71c0f82a097c
igorpadua <igorpadua@tutanota.com>
2023-03-09 10:22:06 -0300

    test: criado testes para transfirmaUmArryDeStringDeCompetenciaEmUmaListaDeCompetencias

---
+++ backend/app/src/test/groovy/com/igor/linketinder/service/CompetenciaServiceTest.groovy
@@ -0,0 +1,35 @@
+package com.igor.linketinder.service
+
+import com.igor.linketinder.entity.Competencia
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class CompetenciaServiceTest {
+    @Test
+    void testTransformaUmArryDeStringDeCompetenciaEmUmaListaDeCompetencia() {
+        String competencias = "{java,python,c}"
+        List<Competencia> resul = CompetenciaService.transformaUmArryDeStringDeCompetenciaEmUmaListaDeCompetencia(competencias)
+        Assertions.assertEquals(3, resul.size())
+    }
+
+    @Test
+    void testTransformaUmArryDeStringDeCompetenciaEmUmaListaDeCompetenciaVazia() {
+        String competencias = "{}"
+        List<Competencia> resul = CompetenciaService.transformaUmArryDeStringDeCompetenciaEmUmaListaDeCompetencia(competencias)
+        Assertions.assertEquals(0, resul.size())
+    }
+
+    @Test
+    void testTransformaUmArryDeStringDeCompetenciaEmUmaListaDeCompetenciaComEspaco() {
+        String competencias = "{java, python, c}"
+        List<Competencia> resul = CompetenciaService.transformaUmArryDeStringDeCompetenciaEmUmaListaDeCompetencia(competencias)
+        Assertions.assertEquals(3, resul.size())
+    }
+
+    @Test
+    void testTransformaUmArryDeStringDeCompetenciaEmUmaListaDeCompetenciaComoNomeIncorreto() {
+        String competencias = "{ JAVA, PYTHON, C }"
+        List<Competencia> resul = CompetenciaService.transformaUmArryDeStringDeCompetenciaEmUmaListaDeCompetencia(competencias)
+        Assertions.assertEquals(3, resul.size())
+    }
+}
