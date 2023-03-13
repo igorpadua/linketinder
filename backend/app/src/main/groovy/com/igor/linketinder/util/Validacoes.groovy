c79cc3eb8ec530da1adc6dd7f5c19857e1ac914c
igorpadua <igorpadua@tutanota.com>
2023-03-13 12:31:07 -0300

    fix: criado uma classe Validacoes para validar dados que o usuario escreve

---
+++ backend/app/src/main/groovy/com/igor/linketinder/util/Validacoes.groovy
@@ -0,0 +1,27 @@
+package com.igor.linketinder.util
+
+import java.util.regex.Pattern
+
+class Validacoes {
+    static boolean validaCEP(String cep) {
+        if (cep == null || cep.isEmpty()) return false
+        Pattern cepRegex = ~/^\d{5}-\d{3}$/
+        if (!cepRegex.matcher(cep).matches()) return false
+        return true
+    }
+
+    static Boolean validaCNPJ(String cnpj) {
+        if (cnpj == null || cnpj.isEmpty()) return false
+        final Pattern cnpjRegex = ~/^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/
+        if (!cnpjRegex.matcher(cnpj).matches()) return false
+        return true
+    }
+
+    static Boolean validaCPF(String cpf) {
+        if (cpf == null || cpf.isEmpty()) return false
+        final Pattern cpfRegex = ~/^\d{3}\.\d{3}\.\d{3}-\d{2}$/
+        if (!cpfRegex.matcher(cpf).matches()) return false
+        return true
+    }
+
+}
--- backend/app/src/main/groovy/com/igor/linketinder/view/PessoaView.groovy
+++ backend/app/src/main/groovy/com/igor/linketinder/view/PessoaView.groovy
@@ -5,31 +5,12 @@ import com.igor.linketinder.model.Pessoa
 import java.util.regex.Pattern
 
 class PessoaView {
-    static boolean validaCEP(String cep) {
-        if (cep == null || cep.isEmpty()) return false
-        Pattern cepRegex = ~/^\d{5}-\d{3}$/
-        if (!cepRegex.matcher(cep).matches()) return false
-        return true
-    }
-
-    static String pegaCep() {
-        Scanner scanner = new Scanner(System.in)
-        print("Digite o CEP da empresa: ")
-        String cep = scanner.nextLine()
-        while (!validaCEP(cep)) {
-            println("CEP inválido")
-            print("Digite o CEP da empresa: ")
-            cep = scanner.nextLine()
-        }
-        return cep
-    }
-
-    private static Boolean listaEstaValida(List<Pessoa> pessoas) {
-        return pessoas != null && !pessoas.isEmpty()
-    }
 
     static void imprimir(List<Pessoa> pessoas) {
-        if (!listaEstaValida(pessoas)) return
+        if (pessoas.isEmpty()) {
+            println("Nenhuma pessoa cadastrada")
+            return
+        }
 
         for (pessoa in pessoas) {
             println(pessoa)
