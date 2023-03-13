7f3affa63769ca90971c442308b47cafba583745
igorpadua <igorpadua@tutanota.com>
2023-03-13 12:31:32 -0300

    fix: CompetenciaView não faz o que não deve

--- backend/app/src/main/groovy/com/igor/linketinder/view/CompetenciaView.groovy
+++ backend/app/src/main/groovy/com/igor/linketinder/view/CompetenciaView.groovy
@@ -6,58 +6,7 @@ import com.igor.linketinder.model.Competencia
 @TypeChecked
 class CompetenciaView {
 
-    static ArrayList<Competencia> escolherCompetencias() {
-        boolean finalizarEscolhas = true
-        List<Competencia> listasCompetencias = new ArrayList<>()
-        Scanner scanner = new Scanner(System.in)
-
-        while (finalizarEscolhas) {
-            menuEscolhaCompetencias()
-            int opc = scanner.nextInt()
-            switch (opc) {
-                case 1:
-                   listasCompetencias.add(Competencia.python)
-                    break
-                case 2:
-                    listasCompetencias.add(Competencia.java)
-                    break
-                case 3:
-                    listasCompetencias.add(Competencia.springframework)
-                    break
-                case 4:
-                    listasCompetencias.add(Competencia.angular)
-                    break
-                case 5:
-                    listasCompetencias.add(Competencia.cplusplus)
-                    break
-                case 6:
-                    listasCompetencias.add(Competencia.c)
-                    break
-                case 7:
-                    listasCompetencias.add(Competencia.javascript)
-                    break
-                case 8:
-                    listasCompetencias.add(Competencia.html)
-                    break
-                case 9:
-                    listasCompetencias.add(Competencia.node)
-                    break
-                case 10:
-                    if (listasCompetencias.isEmpty()) {
-                        println("Nenhuma competencia adicionada")
-                        break
-                    }
-                    finalizarEscolhas = false
-                    break
-                default:
-                    println("Opção incorreta")
-            }
-
-        }
-        return listasCompetencias
-    }
-
-    private static void menuEscolhaCompetencias() {
+    static void menuEscolhaCompetencias() {
         println("Escolhas as competencias: ")
         println("1 - Python")
         println("2 - Java")
